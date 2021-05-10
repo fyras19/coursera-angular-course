d6f204c78c8ec2811cac6d043ebee7d8735915fa
Firas Yahyaoui <firas.yahyaoui@supcom.tn>
2021-05-11 00:56:45 +0100

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -64,7 +65,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     ReactiveFormsModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [
     DishService,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -13,14 +15,40 @@ import { DishService } from '../services/dish.service';
 })
 export class DishdetailComponent implements OnInit {
 
+  commentForm: FormGroup;
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  newComment: Comment;
+  @ViewChild('fform') commentFormDirective;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Author name is required',
+      'minlength': 'Author name must be at least 2 characters long',
+    },
+    'comment': {
+      'required': 'Comment is required'
+    }
+  };
 
   constructor(private dishService: DishService,
     private location: Location,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private fb: FormBuilder) {
+    this.createForm();
+    this.newComment = {
+      author: '',
+      date: new Date().toISOString(),
+      comment: '',
+      rating: 5
+    }
   }
 
   ngOnInit(): void {
@@ -41,6 +69,52 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      'author': ['', [Validators.required, Validators.minLength(2)]],
+      'rating': 5,
+      'comment': ['', Validators.required]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanges(data));
+    
+    this.onValueChanges();
+  }
+
+  onSubmit(): void {
+    this.newComment.author = this.commentForm.value.author;
+    this.newComment.comment = this.commentForm.value.comment;
+    this.newComment.rating = this.commentForm.value.rating;
+    this.newComment.date = new Date().toISOString();
+    this.dish.comments.push(this.newComment);
+    this.commentFormDirective.resetForm();
+    this.commentForm.reset({
+      'author': '',
+      'rating': 5,
+      'comment': ''
+    });
+  }
+
+  onValueChanges(data?): void {
+    if(!this.commentForm) return;
+    console.log(this.formErrors);
+    const form = this.commentForm;
+    for(const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if(control && control.dirty && control.invalid){
+          const messages = this.validationMessages[field];
+          for(const message in messages){
+            if(control.errors.hasOwnProperty(message))
+              this.formErrors[field] += messages[message] + ' '; 
+          }
+        }
+      }
+    }
   }
 
 }
